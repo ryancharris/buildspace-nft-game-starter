08093ccdb329f4a6c5c75dfe2d1c709ff20850ed
Ryan Harris <harrisryan1@gmail.com>
2021-10-30 16:01:43 -0400

    add gitignore and connect button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import React from 'react';
-import twitterLogo from './assets/twitter-logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import twitterLogo from "./assets/twitter-logo.svg";
+import "./App.css";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "ryan_c_harris";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [account, setAccount] = useState(null);
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Please install a web3 wallet");
+        return;
+      }
+
+      // Ask for access to wallet's account
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected!", accounts[0]);
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const checkForWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        // The user has no wallet
+        console.log("You need to connect a web3 wallet!");
+        return;
+      } else {
+        // The user has a web3 wallet in their browser
+        console.log("We detect you have a web3 wallet!");
+
+        // Fetch the accounts in the wallet
+        const accounts = await ethereum.request({
+          method: "eth_accounts",
+        });
+
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          setAccount(account);
+          console.log("Added account from your wallet:", account);
+        } else {
+          console.log("No accounts found in your wallet...");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    checkForWallet();
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
-          <p className="sub-text">Team up to protect the Metaverse!</p>
+          <p className="header gradient-text">Final Fantasy</p>
+          <p className="header gradient-text">⚔️</p>
+          <p className="header gradient-text">Blockchain</p>
+          <p className="sub-text">
+            Sepiroth has invaded the metaverse and we must stop him!
+          </p>
           <div className="connect-wallet-container">
             <img
-              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-              alt="Monty Python Gif"
+              src="https://media.giphy.com/media/xUNda4iGoWk9YqKS0U/giphy.gif"
+              alt="Final Fantasy VII GIF"
             />
+            <button
+              className="cta-button connect-wallet-button"
+              onClick={connectWallet}
+            >
+              Connect Wallet To Get Started
+            </button>
           </div>
         </div>
         <div className="footer-container">
@@ -27,7 +93,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built with @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
